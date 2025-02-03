023ed4ea2850c11c84e6e03b14718e1f4bf15045
Juseonghyun <jucoding99@gmail.com>
2025-02-04 00:41:46 +0900

    feat: Hilt EntryPoint 설정

--- feature/main/src/main/java/com/juco/feature/main/MainActivity.kt
+++ feature/main/src/main/java/com/juco/feature/main/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.juco.feature.main.ui.theme.SimplewagecalculatorTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
