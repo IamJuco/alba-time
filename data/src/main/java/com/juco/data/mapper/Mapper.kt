4f490cec0bf25255b7a5096b7c1aaefd92dea066
Juseonghyun <jucoding99@gmail.com>
2025-02-12 19:10:53 +0900

    feat: 캘린더 날짜 Mapper, formatter, 도메인 모델 추가

--- data/src/main/java/com/juco/data/mapper/Mapper.kt
+++ data/src/main/java/com/juco/data/mapper/Mapper.kt
@@ -2,15 +2,33 @@ package com.juco.data.mapper
 
 import com.juco.data.local.entity.WorkPlaceEntity
 import com.juco.domain.model.WorkPlace
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 fun WorkPlaceEntity.toDomain() = WorkPlace(
     id = this.id,
     name = this.name,
-    wage = this.wage
+    wage = this.wage,
+    workDays = this.workDays.toLocalDateList()
 )
 
 fun WorkPlace.toEntity() = WorkPlaceEntity(
     id = this.id,
     name = this.name,
-    wage = this.wage
+    wage = this.wage,
+    workDays = this.workDays.toDateString()
 )
+
+// "yyyy-MM-dd" 포맷
+private val formatter = DateTimeFormatter.ISO_LOCAL_DATE
+
+fun String.toLocalDateList(): List<LocalDate> {
+    return if (this.isEmpty()) emptyList()
+    else this.split(",").mapNotNull { dateString ->
+        runCatching { LocalDate.parse(dateString, formatter) }.getOrNull()
+    }
+}
+
+fun List<LocalDate>.toDateString(): String {
+    return this.joinToString(",") { it.format(formatter) }
+}
--- domain/src/main/java/com/juco/domain/model/WorkPlace.kt
+++ domain/src/main/java/com/juco/domain/model/WorkPlace.kt
@@ -1,7 +1,10 @@
 package com.juco.domain.model
 
+import java.time.LocalDate
+
 data class WorkPlace(
     val id: Int = 0,
     val name: String,
-    val wage: Int
+    val wage: Int,
+    val workDays: List<LocalDate>
 )
