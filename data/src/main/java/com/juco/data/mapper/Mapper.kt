57ff3548707b82afe17522911b3d6ea993144fbd
Juseonghyun <jucoding99@gmail.com>
2025-02-13 20:07:05 +0900

    feat: Formatter 파일로 분리

--- data/src/main/java/com/juco/data/mapper/Mapper.kt
+++ data/src/main/java/com/juco/data/mapper/Mapper.kt
@@ -1,9 +1,9 @@
 package com.juco.data.mapper
 
 import com.juco.data.local.entity.WorkPlaceEntity
+import com.juco.data.util.toDateString
+import com.juco.data.util.toLocalDateList
 import com.juco.domain.model.WorkPlace
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 
 fun WorkPlaceEntity.toDomain() = WorkPlace(
     id = this.id,
@@ -18,17 +18,3 @@ fun WorkPlace.toEntity() = WorkPlaceEntity(
     wage = this.wage,
     workDays = this.workDays.toDateString()
 )
-
-// "yyyy-MM-dd" 포맷
-private val formatter = DateTimeFormatter.ISO_LOCAL_DATE
-
-fun String.toLocalDateList(): List<LocalDate> {
-    return if (this.isEmpty()) emptyList()
-    else this.split(",").mapNotNull { dateString ->
-        runCatching { LocalDate.parse(dateString, formatter) }.getOrNull()
-    }
-}
-
-fun List<LocalDate>.toDateString(): String {
-    return this.joinToString(",") { it.format(formatter) }
-}
