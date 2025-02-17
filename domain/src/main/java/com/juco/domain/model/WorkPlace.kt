9d49e2da025390dae96078e391f7933a07c820f1
Juseonghyun <jucoding99@gmail.com>
2025-02-18 03:30:03 +0900

    feat: WorkPlace에 payDay추가

--- domain/src/main/java/com/juco/domain/model/WorkPlace.kt
+++ domain/src/main/java/com/juco/domain/model/WorkPlace.kt
@@ -6,5 +6,6 @@ data class WorkPlace(
     val id: Int = 0,
     val name: String,
     val wage: Int,
-    val workDays: List<LocalDate>
+    val workDays: List<LocalDate>,
+    val payDay: PayDay
 )
