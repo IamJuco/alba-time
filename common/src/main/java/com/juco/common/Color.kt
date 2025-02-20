c0d7e761273b8b8d8347f486bad6f50721bb63dc
Juseonghyun <jucoding99@gmail.com>
2025-02-20 23:12:23 +0900

    design: 다방면에서 사용할 Color값 설정

---
+++ common/src/main/java/com/juco/common/Color.kt
@@ -0,0 +1,27 @@
+package com.juco.common
+
+import androidx.compose.ui.graphics.Color
+
+val Red = Color(0XFFE57373)
+val Pink = Color(0XFFF06292)
+val Purple = Color(0XFFBA68C8)
+val Blue = Color(0XFF64B5F6)
+val Teal = Color(0XFF4DB6AC)
+val Green = Color(0XFF81C784)
+val Yellow = Color(0XFFFFD54F)
+val Orange = Color(0XFFFFB74D)
+val Brown = Color(0XFFA1887F)
+
+val colorList = listOf(
+    Color(0XFFE57373), // Red
+    Color(0XFFF06292), // Pink
+    Color(0XFFBA68C8), // Purple
+    Color(0XFF64B5F6), // Blue
+    Color(0XFF4DB6AC), // Teal
+    Color(0XFF81C784), // Green
+    Color(0XFFFFD54F), // Yellow
+    Color(0XFFFFB74D), // Orange
+    Color(0XFFA1887F)  // Brown
+)
+
+val randomColor = colorList.random()
