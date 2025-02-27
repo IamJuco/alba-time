e787c6b6f7476aed59b711b7f7c9af5bf76d80b6
Juseonghyun <jucoding99@gmail.com>
2025-02-27 22:29:37 +0900

    feat: Room DB AutoMigration 설정

--- data/src/main/java/com/juco/data/local/database/AppDatabase.kt
+++ data/src/main/java/com/juco/data/local/database/AppDatabase.kt
@@ -1,5 +1,6 @@
 package com.juco.data.local.database
 
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
@@ -7,8 +8,18 @@ import com.juco.data.local.dao.WorkPlaceDao
 import com.juco.data.local.entity.WorkPlaceEntity
 import com.juco.data.util.WorkPlaceConverters
 
-@Database(entities = [WorkPlaceEntity::class], version = 1, exportSchema = false)
+@Database(
+    version = 2,
+    entities = [WorkPlaceEntity::class],
+    exportSchema = true,
+    autoMigrations = [AutoMigration(from = 1, to = 2)]
+)
 @TypeConverters(WorkPlaceConverters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun workPlaceDao(): WorkPlaceDao
 }
+
+// 칼럼 추가시 version 올리고 from, to 하나씩 올리기
+// ex
+// version = 3
+// autoMigrations = [AutoMigration(from = 2, to = 3)]
