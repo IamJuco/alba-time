4a817f3fb01371ff4958e6a30f6d5b9c71ee60a1
Juseonghyun <jucoding99@gmail.com>
2025-03-01 04:58:05 +0900

    refactor: adMobs 초기화를 백그라운드 Thread에서 초기화되도록 리팩토링
    Ui블로킹을 막기위해 리팩토링

--- app/src/main/java/com/juco/albatime/app/AlbaTimeApplication.kt
+++ app/src/main/java/com/juco/albatime/app/AlbaTimeApplication.kt
@@ -3,11 +3,17 @@ package com.juco.albatime.app
 import android.app.Application
 import com.google.android.gms.ads.MobileAds
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @HiltAndroidApp
 class AlbaTimeApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        MobileAds.initialize(this)
+        val backgroundScope = CoroutineScope(Dispatchers.IO)
+        backgroundScope.launch {
+            MobileAds.initialize(this@AlbaTimeApplication) {}
+        }
     }
 }
