5d67be350e215578d8ed741d860d29bcce62ace0
Juseonghyun <jucoding99@gmail.com>
2025-03-05 14:05:09 +0900

    chore: versionCode및 Name 설정
    vesionCode는 0.8.0, 베타버전 이기에 80으로 설정, versionName은 <major>.<minor>.<point> 형식에 준수

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.juco.albatime"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.compileSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 80
+        versionName = "0.8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
