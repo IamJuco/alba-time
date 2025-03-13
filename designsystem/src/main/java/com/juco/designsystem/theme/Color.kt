8b316734123e0f5e526c957f0d88abbad09df685
Juseonghyun <jucoding99@gmail.com>
2025-03-14 03:34:30 +0900

    feat: color 값및 ColorScheme 값 설정

--- designsystem/src/main/java/com/juco/designsystem/theme/Color.kt
+++ designsystem/src/main/java/com/juco/designsystem/theme/Color.kt
@@ -10,6 +10,12 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+
+// 진한 레드
+val DeepRed = Color(0xFFEE0000)
+
 // 근무지 카드 컬러
 val Red = Color(0XFFE57373)
 val Pink = Color(0XFFF06292)
