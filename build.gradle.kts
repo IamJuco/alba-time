080a5ddc9037f3968a8930b8a1fddf9075059bb3
Juseonghyun <jucoding99@gmail.com>
2025-04-13 00:48:26 +0900

    chore: 모든 모듈에서 detekt와 ktlint 플러그인 사용할 수 있도록 설정및 커스텀 detekt 사용할 수 있도록 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,3 +10,23 @@ plugins {
     alias(libs.plugins.ktlint) apply false
     alias(libs.plugins.detekt) apply false
 }
+
+// 모든 프로젝트에 적용할 플러그인
+subprojects {
+    // 루트 프로젝트는 굳이.. 제외
+    if (project == rootProject) return@subprojects
+
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    // Detekt
+    extensions.configure<io.gitlab.arturbosch.detekt.extensions.DetektExtension> {
+        config = files("$rootDir/detekt.yml")
+        buildUponDefaultConfig = true
+    }
+
+    // KtLint
+    extensions.configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        android.set(true)
+    }
+}
