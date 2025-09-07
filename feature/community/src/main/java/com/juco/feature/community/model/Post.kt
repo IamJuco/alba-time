de2be5dceafb0cee4165f697643e5c162c0eea06
Juseonghyun <jucoding99@gmail.com>
2025-09-07 20:41:16 +0900

    [feat/#31] Post model에 body와 boardType 추가

--- feature/community/src/main/java/com/juco/feature/community/model/Post.kt
+++ feature/community/src/main/java/com/juco/feature/community/model/Post.kt
@@ -2,8 +2,10 @@ package com.juco.feature.community.model
 
 data class Post(
     val id: String,
+    val author: String,
     val title: String,
-    val author: String = "",
+    val body: String,
+    val boardType: String? = "",
     val likeCount: Int = 0,
     val commentCount: Int = 0
 )
